35b6161c004a594e06c09eec07054d7728316c77
adioz <adiozdaniel@gmail.com>
2025-01-12 15:42:09 +0300

    feat: add pearson correlation coefficient arithmetic - Returns a value with ten decimal places

---
+++ pkg/pearson_correlation.go
@@ -0,0 +1,25 @@
+package pkg
+
+import "math"
+
+func PearsonCorrelation(x, y []float64) float64 {
+	var n = float64(len(x))
+	var sumX, sumY, sumXY, sumX2, sumY2 float64
+
+	for i := 0; i < len(x); i++ {
+		sumX += x[i]
+		sumY += y[i]
+		sumXY += x[i] * y[i]
+		sumX2 += x[i] * x[i]
+		sumY2 += y[i] * y[i]
+	}
+
+	numerator := (n * sumXY) - (sumX * sumY)
+	denominator := math.Sqrt((n*sumX2)-(sumX*sumX)) * ((n * sumY2) - (sumY * sumY))
+
+	if denominator == 0 {
+		return 0
+	}
+
+	return numerator / denominator
+}
