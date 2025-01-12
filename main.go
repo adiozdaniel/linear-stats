f1ca6025bbd2acdb332b055d8d9a0db3326be977
adioz <adiozdaniel@gmail.com>
2025-01-12 15:06:17 +0300

    feat: read text file contents

--- main.go
+++ main.go
@@ -7,7 +7,22 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("Wrong Usage, check documentation")
+		fmt.Println("😕 Wrong Usage, check documentation")
 		return
 	}
+
+	fileName := os.Args[1]
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Printf("Oops, we couldn't locate your file 🥺\nCheck your path and try again\n")
+		return
+	}
+	defer file.Close()
+
+	contents, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("😱 Error reading file...")
+		return
+	}
+	fmt.Printf("File contents: %s\n", contents)
 }
