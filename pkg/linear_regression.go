73ab2fdd84f0801fa3b18493d261f618370a8edf
adioz <adiozdaniel@gmail.com>
2025-01-12 15:33:19 +0300

    feat: add linear regression calculator - It returns a six decimal value

---
+++ pkg/linear_regression.go
@@ -0,0 +1,17 @@
+package pkg
+
+func LinearRegression(x, y []float64) (float64, float64) {
+	var n = float64(len(x))
+	var sumX, sumY, sumXY, sumX2 float64
+
+	for i := 0; i < len(x); i++ {
+		sumX += x[i]
+		sumY += y[i]
+		sumXY += x[i] * y[i]
+		sumX2 += x[i] * x[i]
+	}
+
+	b1 := (n*sumXY - sumX*sumY) / (n*sumX2 - sumX*sumX)
+	b0 := (sumY - b1*sumX) / n
+	return b0, b1
+}
